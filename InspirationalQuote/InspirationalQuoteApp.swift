b7b7f087b764cd9566a6643f2014372ae646321d
Yeseo Kim <YKim@lcs.on.ca>
2021-02-19 12:05:42 -0500

    Initial Commit

---
+++ InspirationalQuote/InspirationalQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspirationalQuoteApp.swift
+//  InspirationalQuote
+//
+//  Created by Yeseo Kim on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct InspirationalQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
